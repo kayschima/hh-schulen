16350352f602286a5d55b2e5fd2bb6570682f7ff
Kay <kayschima@web.de>
2025-07-31 22:28:35 +0200

    show dialog for details

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { Button } from '@/components/ui/button';
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Head, router } from '@inertiajs/vue3';
@@ -38,7 +39,7 @@ watchDebounced(
         <link href="https://rsms.me/" rel="preconnect" />
         <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
     </Head>
-    <div class="mx-auto bg-background p-6 text-primary lg:max-w-7xl">
+    <div class="mx-auto min-h-screen bg-background p-6 text-primary lg:max-w-7xl">
         <header class="m-2 w-full rounded-2xl border border-2 border-primary p-8">
             <nav>
                 <div>
@@ -57,24 +58,94 @@ watchDebounced(
                 <TableCaption>Seite {{ schools.current_page }} von {{ schools.last_page }} - insgesamt {{ schools.total }} Datensätze </TableCaption>
                 <TableHeader>
                     <TableRow>
-                        <TableHead>Schulnummer</TableHead>
+                        <TableHead></TableHead>
                         <TableHead>Name</TableHead>
+                        <TableHead>Schulnummer</TableHead>
                         <TableHead>Adresse + Ort</TableHead>
                         <TableHead>Schultyp</TableHead>
                         <TableHead>Bezirk</TableHead>
-                        <TableHead class="text-right"></TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
                     <TableRow v-for="school in schools.data" :key="school.id">
-                        <TableCell>{{ school.schul_id }}</TableCell>
+                        <TableCell class="text-right">
+                            <Dialog>
+                                <DialogTrigger as-child>
+                                    <Button class="rounded-full" variant="outline">...</Button>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <form>
+                                        <DialogHeader class="mb-6">
+                                            <DialogTitle>{{ school.schulname }}</DialogTitle>
+                                        </DialogHeader>
+
+                                        <Table class="mb-6">
+                                            <TableBody>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Schulnummer:</TableCell>
+                                                    <TableCell>{{ school.schul_id }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Schulname:</TableCell>
+                                                    <TableCell>{{ school.schulname }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Straße:</TableCell>
+                                                    <TableCell>{{ school.adresse_strasse_hausnr }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">PLZ + Ort:</TableCell>
+                                                    <TableCell>{{ school.adresse_ort }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Telefon:</TableCell>
+                                                    <TableCell>{{ school.schul_telefonnr }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Fax:</TableCell>
+                                                    <TableCell>{{ school.fax }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Bezirk:</TableCell>
+                                                    <TableCell>{{ school.bezirk }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Rechtsform:</TableCell>
+                                                    <TableCell>{{ school.rechtsform }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Schultyp:</TableCell>
+                                                    <TableCell>{{ school.kapitelbezeichnung }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Schulformen:</TableCell>
+                                                    <TableCell v-html="school.schulform?.split('|').join(',<br>')" />
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Ganztagsform:</TableCell>
+                                                    <TableCell>{{ school.ganztagsform }}</TableCell>
+                                                </TableRow>
+                                                <TableRow>
+                                                    <TableCell class="font-bold">Abschlüsse:</TableCell>
+                                                    <TableCell v-html="school.abschluss?.split('|').join(',<br>')" />
+                                                </TableRow>
+                                            </TableBody>
+                                        </Table>
+
+                                        <DialogFooter>
+                                            <DialogClose as-child>
+                                                <Button> Schließen</Button>
+                                            </DialogClose>
+                                        </DialogFooter>
+                                    </form>
+                                </DialogContent>
+                            </Dialog>
+                        </TableCell>
                         <TableCell>{{ school.schulname }}</TableCell>
+                        <TableCell>{{ school.schul_id }}</TableCell>
                         <TableCell>{{ school.adresse_strasse_hausnr }}, {{ school.adresse_ort }}</TableCell>
                         <TableCell>{{ school.kapitelbezeichnung }}</TableCell>
                         <TableCell>{{ school.bezirk }}</TableCell>
-                        <TableCell class="text-right">
-                            <Button as="a" class="rounded-full" size="sm" variant="outline">...</Button>
-                        </TableCell>
                     </TableRow>
                 </TableBody>
             </Table>
